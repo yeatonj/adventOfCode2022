14b320fbfb0c6e87c569cf20e1d286c13ec3295f
Josh Yeaton <yeaton.josh@gmail.com>
2022-12-02 10:29:51 -0500

    Completed Part 2 file and fixed a bug with part 1 file where function inputs were swapped around

--- day2/RockPaperScissorsPartTwo.java
+++ day2/RockPaperScissorsPartTwo.java
@@ -1,5 +1,6 @@
 // Program to calculate total score for the strategy guide in a game of
-// rock paper scissors
+// rock paper scissors in which you must win, lose, or tie based on a strategy
+// guide
 
 // Written by Joshua Yeaton
 // Written on 12/2/22
@@ -10,7 +11,7 @@ import java.io.FileNotFoundException;
 import java.util.Map;
 import java.util.HashMap;
 
-public class RockPaperScissors {
+public class RockPaperScissorsPartTwo {
   public static void main(String[] args) {
     // Create the maps
     Map<String,String> throwMap = makeThrowMap();
@@ -33,8 +34,8 @@ public class RockPaperScissors {
     int myTotalScore = 0;
     while (stratScanner.hasNextLine()) {
       String currentGame = stratScanner.nextLine();
+      String myThrow = throwMap.get(currentGame);
       String oppThrow = throwMap.get(currentGame.substring(0,1));
-      String myThrow = throwMap.get(currentGame.substring(2));
       myTotalScore += scoreGame(oppThrow, myThrow, scoreMap);
     }
     System.out.println("Total score is: " + myTotalScore);
@@ -93,18 +94,31 @@ public class RockPaperScissors {
     final String oppPaper = "B";
     final String oppScissors = "C";
 
-    final String myRock = "X";
-    final String myPaper = "Y";
-    final String myScissors = "Z";
+    // X means we need to lose, Y means tie, Z means win
+    final String myRock1 = "B X";
+    final String myRock2 = "A Y";
+    final String myRock3 = "C Z";
+    final String myPaper1 = "C X";
+    final String myPaper2 = "B Y";
+    final String myPaper3 = "A Z";
+    final String myScissors1 = "A X";
+    final String myScissors2 = "C Y";
+    final String myScissors3 = "B Z";
 
     // Create the hashmap that maps throws to scores
     Map<String,String> throwMap = new HashMap<>();
     throwMap.put(oppRock, "rock");
     throwMap.put(oppPaper, "paper");
     throwMap.put(oppScissors, "scissors");
-    throwMap.put(myRock, "rock");
-    throwMap.put(myPaper, "paper");
-    throwMap.put(myScissors, "scissors");
+    throwMap.put(myRock1, "rock");
+    throwMap.put(myRock2, "rock");
+    throwMap.put(myRock3, "rock");
+    throwMap.put(myPaper1, "paper");
+    throwMap.put(myPaper2, "paper");
+    throwMap.put(myPaper3, "paper");
+    throwMap.put(myScissors1, "scissors");
+    throwMap.put(myScissors2, "scissors");
+    throwMap.put(myScissors3, "scissors");
     return throwMap;
   }
 }
