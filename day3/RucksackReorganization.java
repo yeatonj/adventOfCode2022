1b9300a1a7e9ea4de80a8098a85895b0c9fb1081
Josh Yeaton <yeaton.josh@gmail.com>
2022-12-03 09:15:36 -0500

    Initialized day 3 files

---
+++ day3/RucksackReorganization.java
@@ -0,0 +1,9 @@
+// File to check (and possibly sort) rucksacks for Advent of Code 2022 Day 3
+// Written by: Josh Yeaton
+// Written on 12/3/2022
+
+class RucksackReorganization {
+  public static void main(String[] args) {
+    System.out.println("Test");
+  }
+}
