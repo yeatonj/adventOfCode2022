9d08d6572365053cab339ab38afd38a175616423
Josh Yeaton <yeaton.josh@gmail.com>
2022-12-05 09:55:04 -0500

    Completed functions including print function for printing cargo

--- day5/SupplyStacks.java
+++ day5/SupplyStacks.java
@@ -13,25 +13,39 @@ class SupplyStacks {
   public static void main(String[] args) {
     String startCargo = "/Users/yeato/Documents/git_projects/adventOfCode2022/day5/cargo_start.txt";
     String instructions = "/Users/yeato/Documents/git_projects/adventOfCode2022/day5/cargo_instructions.txt";
-    ArrayList<ArrayList<Character>> testFile = createCargoList(startCargo);
+    ArrayList<ArrayList<Character>> cargoArray = createCargoList(startCargo);
+
+    printContents(cargoArray);
+    moveCrates(3,1,2,cargoArray);
+    printContents(cargoArray);
+
   }
 
   // Method to pop a certain number of elements from a stack of cargo,
-  // returning those elements
+  // returning those elements (and modifying the original arrayList)
   private static ArrayList<Character> popEnd(ArrayList<Character> listIn,
                                             int elsToPop) {
-    // To be implemented
-    return null;
+    // Add the popped elements to a new list
+    ArrayList<Character> tempList = new ArrayList<>();
+    for (int i = listIn.size() - elsToPop; i < listIn.size(); i++) {
+      tempList.add(listIn.get(i));
+    }
+    // Then, remove the elements from the original arrayList
+    listIn.subList(listIn.size() - elsToPop, listIn.size()).clear();
+
+    return tempList;
   }
 
   // Method to move a number of crates from one stack to another in a given
-  // stack of cargo containers
+  // stack of cargo containers, note that stack 1 is indexed at index 0, etc
   private static void moveCrates(int numCrates,
                                 int sourceStack,
                                 int destStack,
                                 ArrayList<ArrayList<Character>> stackList) {
-    // To be implemented
-
+    ArrayList<Character> tempList = popEnd(stackList.get(sourceStack - 1), numCrates);
+    for (int i = 0; i < numCrates; i++) {
+      stackList.get(destStack - 1).add(tempList.get(i));
+    }
   }
 
   // Method to create the cargo stacks from an input text file, these stacks
@@ -62,7 +76,7 @@ class SupplyStacks {
     }
     stackScannerNumCrates.close();
     // Create the appropriate number of stacks
-    for (int i = 0; i < currLine.length()/ + 1; i++) {
+    for (int i = 0; i < currLine.length()/4 + 1; i++) {
       cargoStacks.add(new ArrayList<Character>());
     }
 
@@ -80,12 +94,18 @@ class SupplyStacks {
     // Finally, reverse each of the lists to make the stacks correct
     for (int k = 0; k < cargoStacks.size(); k++) {
       Collections.reverse(cargoStacks.get(k));
-      // System.out.println("Cargo bucket " + k);
-      // for (int l = 0; l < cargoStacks.get(k).size(); l++) {
-      //   System.out.println(cargoStacks.get(k).get(l));
-      // }
     }
 
     return cargoStacks;
+  }
+
+  // Function to print the cargo manifest
+  private static void printContents(ArrayList<ArrayList<Character>> cargoList) {
+    for (int i = 1; i < cargoList.size() + 1; i++) {
+      System.out.println("Contents of Stack " + i + ", top to bottom:");
+      for (int j = cargoList.get(i-1).size() - 1; j >= 0; j --) {
+        System.out.println(cargoList.get(i-1).get(j));
+      }
+    }
   }
 }
