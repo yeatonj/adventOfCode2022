bb7eca82725e99ac0ebe77366f8c958807218276
Josh Yeaton <yeaton.josh@gmail.com>
2022-12-05 08:13:00 -0500

    Added Day 5 files

---
+++ day5/SupplyStacks.java
@@ -0,0 +1,9 @@
+// File to determine cargo stacking for supply stacks
+// Written by: Josh Yeaton
+// Written on 12/5/2022
+
+class SupplyStacks {
+  public static void main(String[] args) {
+    System.out.println("Test");
+  }
+}
