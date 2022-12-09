13acd0aade98c3da6346cb9a86b1e8cf4f8a4cef
Josh Yeaton <yeaton.josh@gmail.com>
2022-12-09 11:02:18 -0500

    Completed part 1

--- day9/KnotPath.java
+++ day9/KnotPath.java
@@ -13,9 +13,9 @@ public class KnotPath {
 
   // Constructor
   public KnotPath() {
-    path = new ArrayList<>();
+    this.path = new ArrayList<>();
     // Add the origin to the path (all knots start at origin)
-    path.add(new KnotLoc(0,0));
+    this.path.add(new KnotLoc(0,0));
   }
 
   public void genPathFromFile(String filePath) {
@@ -29,8 +29,8 @@ public class KnotPath {
     }
 
     // Set starting locations for the path to generate from
-    int currentX = path.get(path.size()-1).getX();
-    int currentY = path.get(path.size()-1).getY();
+    int currentX = this.path.get(path.size()-1).getX();
+    int currentY = this.path.get(path.size()-1).getY();
 
     while(headPathScanner.hasNext()) {
       // Each line of the file is (direction, magnitude)
@@ -47,13 +47,119 @@ public class KnotPath {
       } else {
         System.out.println("Bad direction, skipping line...");
       }
-      path.add(new KnotLoc(currentX,currentY));
+      this.path.add(new KnotLoc(currentX,currentY));
     }
     headPathScanner.close();
+  }
 
+  // Function to move the tail from its current last location on the path
+  // to a new location given the current location of the head
+  public void genPathFromFileWithTail(String filePath, KnotPath tail) {
+    File knotPathFile = null;
+    Scanner headPathScanner = null;
+    try {
+      knotPathFile = new File(filePath);
+      headPathScanner = new Scanner(knotPathFile);
+    } catch (FileNotFoundException e) {
+      System.out.println("File not found...");
+    }
+
+    // Set starting locations for the path to generate from
+    int currentX = this.path.get(path.size()-1).getX();
+    int currentY = this.path.get(path.size()-1).getY();
+
+    while(headPathScanner.hasNext()) {
+      // Each line of the file is (direction, magnitude)
+      String direction = headPathScanner.next();
+      int magnitude = Integer.parseInt(headPathScanner.next());
+      while (magnitude > 0) {
+        if (direction.equals("U")) {
+          currentY += 1;
+        } else if (direction.equals("D")) {
+          currentY -= 1;
+        } else if (direction.equals("R")) {
+          currentX += 1;
+        } else if (direction.equals("L")) {
+          currentX -= 1;
+        } else {
+          System.out.println("Bad direction, skipping line...");
+        }
+        this.path.add(new KnotLoc(currentX,currentY));
+        this.moveFollower(tail);
+        magnitude--;
+      }
+    }
+    headPathScanner.close();
+  }
+
+  public void moveKnot(int moveX, int moveY) {
+    // Get current locations of the last point
+    int currentX = this.path.get(this.path.size()-1).getX();
+    int currentY = this.path.get(this.path.size()-1).getY();
+
+    // add a new location representing the move
+    this.path.add(new KnotLoc(currentX + moveX, currentY + moveY));
   }
 
   public ArrayList<KnotLoc> getPath() {
-    return path;
+    return this.path;
+  }
+
+  public KnotLoc getCurrentLoc() {
+    return this.path.get(this.path.size() - 1);
+  }
+
+  private void moveFollower(KnotPath tail) {
+    // Get the current location of the tail
+    KnotLoc currTailLoc = tail.getCurrentLoc();
+    KnotLoc currHeadLoc = this.getCurrentLoc();
+
+    // Find the differences in location
+    KnotLoc locDiff = currHeadLoc.minus(currTailLoc);
+    int xDiff = locDiff.getX();
+    int yDiff = locDiff.getY();
+
+    if ((xDiff <= 1 && xDiff >= -1) && (yDiff <= 1 && yDiff >= -1)) {
+      // Don't move the knot
+      tail.moveKnot(0,0);
+    }
+    else if (xDiff == 0) {
+      if (yDiff == 2) {
+        // move the knot up
+        tail.moveKnot(0,1);
+      }
+      else {
+        // move the knot down
+        tail.moveKnot(0,-1);
+      }
+    }
+    else if (yDiff == 0){
+      if (xDiff == 2) {
+        // move the knot right
+        tail.moveKnot(1,0);
+      }
+      else {
+        // move the knot left
+        tail.moveKnot(-1,0);
+      }
+    }
+    else { // diagonal move
+      if (xDiff > 0 && yDiff > 0) {
+        // diagonal up right
+        tail.moveKnot(1,1);
+      }
+      else if (xDiff > 0 && yDiff < 0) {
+        // diagonal down right
+        tail.moveKnot(1,-1);
+      }
+      else if (xDiff < 0 && yDiff > 0) {
+        // diagonal up left
+        tail.moveKnot(-1,1);
+      }
+      else {
+        // diagonal down left
+        tail.moveKnot(-1,-1);
+      }
+    }
   }
 }
--- day9/RopeBridge.java
+++ day9/RopeBridge.java
@@ -12,12 +12,30 @@ public class RopeBridge {
   public static void main(String[] args) {
     String filePath = "/Users/yeato/Documents/git_projects/adventOfCode2022/day9/rope_movement.txt";
 
+    // Generate the head path
     KnotPath headPath = new KnotPath();
-    headPath.genPathFromFile(filePath);
-    ArrayList<KnotLoc> headLocs = headPath.getPath();
-    for (KnotLoc location : headLocs) {
-      System.out.println(location.getX() + ", " + location.getY());
-    }
+    KnotPath tailPath = new KnotPath();
+    headPath.genPathFromFileWithTail(filePath, tailPath);
+    // headPath.genPathFromFile(filePath);
 
+    // Start the tail path
+
+    ArrayList<KnotLoc> headLocs = headPath.getPath();
+    ArrayList<KnotLoc> tailLocs = tailPath.getPath();
+    ArrayList<KnotLoc> uniqueTailLocs = new ArrayList<>();
+    for (int i = 0; i < tailLocs.size(); i++) {
+      System.out.println("Head: " + headLocs.get(i).getX() + ", " + headLocs.get(i).getY());
+      System.out.println("Tail: " + tailLocs.get(i).getX() + ", " + tailLocs.get(i).getY());
+      int j = 0;
+      // Check whether this location is unique
+      while (j < uniqueTailLocs.size() && !uniqueTailLocs.get(j).equals(tailLocs.get(i))) {
+        j++;
+      }
+      // if it is, add it to the list of unique locations
+      if (j == uniqueTailLocs.size()) {
+        uniqueTailLocs.add(tailLocs.get(i));
+      }
+    }
+    System.out.println("Number of unique tail locations: " + uniqueTailLocs.size());
   }
 }
