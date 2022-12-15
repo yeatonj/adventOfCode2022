2128efa592fb22da6251b7dc1baaec3890ee92ae
Josh Yeaton <yeaton.josh@gmail.com>
2022-12-15 15:16:49 -0500

    Completed part 2

--- day15/BeaconExclusionZone.java
+++ day15/BeaconExclusionZone.java
@@ -12,6 +12,9 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.math.BigInteger;
 
 class BeaconExclusionZone {
   public static void main(String[] args) throws FileNotFoundException {
@@ -68,47 +71,72 @@ class BeaconExclusionZone {
     System.out.println("Locations that can't have a beacon: " + noBeacon.size());
     // Guessed 4724228 - correct!
 
-    // Now for part 2 - find the location that we can't have a beacon
-    // Generate a set of the nobeacon locs in a row
-    // For each possible point in that row, check if it's not in the set
-    // If its not, then we've found our point. If it is, then move on to the next
-    // row
+    // Now for part 2 - sort the sensors by their x locations (increasing), then
+    // iterate through each row, grabbing the coordinates that each overlap on.
+    // Check to see if there is a point at which they don't overlap, and if so,
+    // that's the row we want.
 
     int beaconRow = 0;
     int beaconCol = 0;
     int maxRowCol = 4000000;
+    // int maxRowCol = 20; // Test case
     int minRowCol = 0;
+
+    // First, sort the sensors
+    SensorColComparator sensorColCompare = new SensorColComparator();
+    Collections.sort(sensorList, sensorColCompare);
+
+    // Then, loop through each of the rows
+    boolean found = false;
     for (int i = minRowCol; i < maxRowCol; i++) {
-      HashSet<Point> noBeaconLine = new HashSet<>();
-      for (Sensor sen:sensorList) {
-        HashSet<Point> noBeaconSensor = sen.findPointsInRow(i, minRowCol, maxRowCol);
-        if (noBeaconSensor == null) {
-          continue;
-        }
-        for (Point p : noBeaconSensor) {
-          if (!noBeaconLine.contains(p)) {
-            noBeaconLine.add(p);
-          }
+      // List of the sensor coverage on this line - build it out
+      ArrayList<int[]> sensorCoverageLine = new ArrayList<>();
+      for (Sensor s : sensorList) {
+        int[] sensorCoverage = s.findPointsInRow(i, minRowCol, maxRowCol);
+        if (sensorCoverage != null) {
+          sensorCoverageLine.add(sensorCoverage);
         }
       }
-      System.out.println("Running...");
-      // At this point, we have a set of all the places a beacon cannot be in the line
-      // Now, we simply check the size of the set to see if it is full. If it isn't,
-      // we know the point is there and we iterate through to find which one
-      if (noBeaconLine.size() != maxRowCol + 1) {
-        beaconRow = i;
-        // Now, find the column
-        for (int j = minRowCol; j < maxRowCol; j++) {
-          Point p = new Point(j,i);
-          if (!noBeaconLine.contains(p)) {
-            beaconCol = j;
-          }
+      // Set the current row variable
+      beaconRow = i;
+      // Check the first position and set the range for the first sensor
+      beaconCol = 0;
+      // System.out.println("Testing Sensor " + 0 + " on line " + i);
+      // System.out.println("Sensor coverage " + 0 + " to " + beaconCol);
+
+      // Iterate through the rest of the sensors
+      for (int j = 0; j < sensorCoverageLine.size(); j++) {
+        int sensorStart = sensorCoverageLine.get(j)[0];
+        int sensorEnd = sensorCoverageLine.get(j)[1];
+        // System.out.println("Testing Sensor " + j + " on line " + i);
+        // System.out.println("Sensor coverage " + sensorStart + " to " + sensorEnd);
+        // Check to see if the sensor has overlapping coverage
+        if (sensorStart <= beaconCol && sensorEnd >= beaconCol) {
+          beaconCol = sensorEnd;
         }
+        // System.out.println("Total coverage " + 0 + " to " + beaconCol);
       }
+      // System.out.println("Last overlapping index:" + beaconCol);
+      if (beaconCol != maxRowCol) {
+        beaconCol++;
+        break;
+      }
+      // System.out.println("");
     }
+
+    BigInteger bigRow = BigInteger.valueOf(beaconRow);
+    BigInteger bigCol = BigInteger.valueOf(beaconCol);
+    BigInteger freq = BigInteger.valueOf(4000000);
+
+    BigInteger output1 = bigCol.multiply(freq);
+    BigInteger output2 = output1.add(bigRow);
+
+
 
     System.out.println("Beacon is in row " + beaconRow);
     System.out.println("Beacon is in column " + beaconCol);
-    System.out.println("Tuning frequency is: " + (beaconCol*4000000 + beaconRow));
+    System.out.println("Tuning frequency is: " + output2);
+    // Guess of 4237168587415 was too low
+    // Correct guess 13622251246513
   }
 }
--- day15/Sensor.java
+++ day15/Sensor.java
@@ -7,18 +7,21 @@ import java.awt.Point;
 import java.lang.Math;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Comparator;
 
-class Sensor {
+public class Sensor {
   // Instance variables
   private Point sensorLoc;
   private Point closestBeacon;
   private int beaconDistance; // manhattan distance
+  private int sensorCol;
 
   // Constructor
   public Sensor(int sensorX, int sensorY, Point beacon) {
     // Assign the position variables
     this.sensorLoc = new Point(sensorX, sensorY);
     this.closestBeacon = beacon;
+    this.sensorCol = sensorX;
 
     // Calculate the manhattan distance from origin to beacon location
     int beaconX = (int) beacon.getX();
@@ -39,6 +42,10 @@ class Sensor {
 
   public int getCoverageDist() {
     return this.beaconDistance;
+  }
+
+  public int getSensorCol() {
+    return this.sensorCol;
   }
 
   // Returns the points in a row that this sensor covers
@@ -63,9 +70,35 @@ class Sensor {
     return pointList;
   }
 
-  // Returns the points in a row that this sensor covers
-  public HashSet<Point> findPointsInRow(int row, int minX, int maxX) {
-    int rowDist = Math.abs(row - (int) this.sensorLoc.getY()); // This is correctly calculated
+  // // Returns the points in a row that this sensor covers
+  // public HashSet<Point> findPointsInRow(int row, int minX, int maxX) {
+  //   int rowDist = Math.abs(row - (int) this.sensorLoc.getY()); // This is correctly calculated
+  //   // Total number of points in the row
+  //   // Check this closely for errors!
+  //   int numPoints = 0;
+  //   if (this.beaconDistance - rowDist >= 0) {
+  //     numPoints = 2 * (this.beaconDistance - rowDist) + 1;
+  //   }
+  //   if (numPoints == 0) {
+  //     return null;
+  //   }
+  //   // Initialize the arrayList
+  //   HashSet<Point> pointList = new HashSet<>();
+  //   for (int i = (-1*numPoints)/2; i <= numPoints/2; i++) {
+  //     // Add the points to the arraylist and return them
+  //     int xLoc = ((int) this.sensorLoc.getX()) + i;
+  //     if (xLoc <= maxX && xLoc >= minX) {
+  //       Point tempPoint = new Point(xLoc, row);
+  //       pointList.add(tempPoint);
+  //     }
+  //   }
+  //   return pointList;
+  // }
+
+
+  // New function to return the indices of the overlap with a specific row
+  public int[] findPointsInRow(int row, int minX, int maxX) {
+    int rowDist = Math.abs(row - (int) this.sensorLoc.getY());
     // Total number of points in the row
     // Check this closely for errors!
     int numPoints = 0;
@@ -76,15 +109,26 @@ class Sensor {
       return null;
     }
     // Initialize the arrayList
-    HashSet<Point> pointList = new HashSet<>();
-    for (int i = (-1*numPoints)/2; i <= numPoints/2; i++) {
-      // Add the points to the arraylist and return them
-      int xLoc = ((int) this.sensorLoc.getX()) + i;
-      if (xLoc <= maxX && xLoc >= minX) {
-        Point tempPoint = new Point(xLoc, row);
-        pointList.add(tempPoint);
-      }
+    int midpoint = (int) this.sensorLoc.getX();
+    int startCol = (-1*numPoints)/2 + midpoint;
+    int endCol = numPoints/2 + midpoint;
+    if (startCol < minX) {
+      startCol = minX;
     }
-    return pointList;
+    if (endCol > maxX) {
+      endCol = maxX;
+    }
+    return new int[] {startCol, endCol};
   }
+
+
+}
+
+class SensorColComparator implements Comparator<Sensor> {
+    @Override
+    // Comparator for column values
+    public int compare(Sensor a, Sensor b) {
+      return a.getSensorCol() - b.getSensorCol();
+    }
+
 }
