ee265fd24d7c87626c44bff280ff78d4bd3d2e1c
Josh Yeaton <yeaton.josh@gmail.com>
2023-01-01 10:10:47 -0500

    Added day 21 to repository

---
+++ day21/MonkeyMath.java
@@ -0,0 +1,10 @@
+// Program for doing monkeymath
+// Written for AOC 2022 Day 21
+// Written by Josh Yeaton
+// Written on 1/1/2023
+
+class MonkeyMath {
+  public static void main(String[] args) {
+    System.out.println("Test");
+  }
+}
