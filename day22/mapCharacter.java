a3ebb5606618517caff1c10bfc3037a7617c91a4
Josh Yeaton <yeaton.josh@gmail.com>
2023-01-03 10:05:48 -0500

    Updated name

--- day22/mapCharacter.java
+++ day22/mapCharacter.java
@@ -1,13 +1,13 @@
 // Class to represent a character on a map
 
-public class mapCharacter {
+public class MapCharacter {
   // Instance variables
   private int direction; // 0->R, 1->D, 2->L, 3->U
-  private mapGraph currentMap;
+  private MapGraph currentMap;
 
 
   // Constructor
-  mapCharacter(int direction) {
+  MapCharacter(int direction) {
     if (direction > 3 || direction < 0) {
       System.out.println("Direction invalid, direction set to R");
       this.direction = 0;
@@ -21,7 +21,7 @@ public class mapCharacter {
 
   // Getters
   // Getter for map
-  public mapGraph getCurrentMap() {
+  public MapGraph getCurrentMap() {
     return this.currentMap;
   }
 
@@ -32,7 +32,7 @@ public class mapCharacter {
 
   // Setters
   // Setter for map
-  public void setMap(mapGraph newMap) {
+  public void setMap(MapGraph newMap) {
     this.currentMap = newMap;
   }
 
